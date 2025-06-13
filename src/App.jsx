4250b27b0d16b472cdfd7ead74f752fb570ef736
Michael <michaelbrandmeyer99@gmail.com>
2025-06-13 14:45:12 -0700

    can fetch from api, display pokemon as cards in a grid,

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,80 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [cards, setCards] = useState([]);
+  const pokemonSpriteURLs = [];
+  
+
+
+
+  function displayScore(){
+
+  }
+
+  
+
+  function randomNum(){
+    return Math.floor(Math.random() * 100);
+  }
+
+  async function useAPI(){
+    let offset = randomNum();
+    
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=' + offset)
+    .then(response => {
+      response.json().then(data => {
+
+        const fetches = data.results.map((pokemon) => {
+          return fetch(pokemon.url)
+            .then(response => response.json())
+            .then(pokeData => {
+              console.log(pokeData.sprites.front_default);
+              pokemonSpriteURLs.push(pokeData.sprites.front_default);
+            });
+        });
+
+
+        Promise.all(fetches).then(() => {
+          setCards(pokemonSpriteURLs);
+        });
+      });
+    });
+    
+    
+    
+    
+  }
+const hasFetched = useRef(false);
+
+useEffect(() => {
+  if (!hasFetched.current) {
+    useAPI();
+    hasFetched.current = true;
+  }
+}, []);
+
+
+
+  
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <>
+    <div className="card-container">
+        {cards.map((child) => {
+        return <div className="card" key = {child}>
+        <img src={child} alt="" />
+        
+        </div>;
+      })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  </>
   )
 }
 
